9e61024337acc3b8ab67a4453eeb8ad2fc0a2264
Guus Noij <guus.noij@perplex.nl>
2022-08-16 15:42:15 +0200

    added js and css endpoint

--- routes/index.js
+++ routes/index.js
@@ -7,4 +7,12 @@ router.get('/', function(req, res) {
   res.send({visitorId: process.env.MOCKVISITORID ?? '12345678'});
 });
 
+router.get('/personalised-js-and-css', function(req, res) {
+  console.log(req.query.ip);
+  res.send({
+    js: '(function(){alert("Deze alert javascript is gefetched vanuit de UMS Mock api")}();)',
+    css: 'h1{color:#ef4189!important;}'
+  });
+});
+
 module.exports = router;
